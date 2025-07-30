680c83e8346cd4a24d3e2124b3bad3cc73cff6bd
AdityaSinghDevs <adityasingh.devs@gmail.com>
2025-07-30 15:51:03 +0530

    Generator finished

--- tesseract/core/generator.py
+++ tesseract/core/generator.py
@@ -1,4 +1,6 @@
 from typing import Any
+
+from loggers.logger import get_logger
 from config.config import (
     LATENT_BATCH_SIZE,
     GUIDANCE_SCALE,
@@ -13,6 +15,21 @@ from config.config import (
 )
 from shap_e.diffusion.sample import sample_latents
 
+logger = get_logger(__name__, log_file='app.log')
+
+def validate_inputs(prompt : str, model : Any ,
+                     diffusion : Any)->None :
+    if not isinstance(prompt,str) or not prompt.strip():
+        logger.error("Empty or Invalid Prompt given")
+        raise ValueError("The Prompt should be a str and not empty")
+    if not model:
+        logger.error("Model not provided")
+        raise ValueError("Model must be provided and not none")
+    if not diffusion:
+        logger.error("Diffusion not provided")
+        raise ValueError("Diffusion must be provided and not none")
+
+
 def generate_latents( prompt : str, model : Any, 
 diffusion : Any,
 batch_size : int = LATENT_BATCH_SIZE,
@@ -26,21 +43,32 @@ sigma_max : float = SIGMA_MAX,
 sigma_min : float = SIGMA_MIN,
 s_churn : int = S_CHURN)-> Any:
     
-    latents_outputs = sample_latents(
-    batch_size=batch_size,
-    model=model,
-    diffusion=diffusion,
-    guidance_scale=guidance_scale,
-    model_kwargs=dict(texts=[prompt] * batch_size),
-    progress=progress,
-    clip_denoised=clip_denoised,
-    use_fp16=use_fp16,
-    use_karras=use_karras,
-    karras_steps=karras_steps,
-    sigma_min=sigma_min,
-    sigma_max=sigma_max,
-    s_churn=s_churn,
-    )
+    validate_inputs(prompt, model, diffusion)
+    logger.info(f"Inputs Verified, Starting latent generation from prompt : {prompt}")
+    
+    try:
+        latents_outputs = sample_latents(
+        batch_size=batch_size,
+        model=model,
+        diffusion=diffusion,
+        guidance_scale=guidance_scale,
+        model_kwargs=dict(texts=[prompt] * batch_size),
+        progress=progress,
+        clip_denoised=clip_denoised,
+        use_fp16=use_fp16,
+        use_karras=use_karras,
+        karras_steps=karras_steps,
+        sigma_min=sigma_min,
+        sigma_max=sigma_max,
+        s_churn=s_churn,
+        )
+        logger.info(f"LATENTS LOADED SUCCESFULLY FOR PROMPT : {prompt}")
+        
+        return latents_outputs
 
-    return latents_outputs
+    except Exception as e:
+        logger.error(f"ERROR IN GENERATING LATENTS : {e}")
+        raise 
+
+    
 
