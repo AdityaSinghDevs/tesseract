1ac67c43525920326bec5f1c0deca15c69e1b15d
AdityaSinghDevs <adityasingh.devs@gmail.com>
2025-08-04 23:26:19 +0530

    job logic added

--- api/api.py
+++ api/api.py
@@ -1,7 +1,8 @@
 from typing import Union
 import os
+import uuid
 
-from fastapi import FastAPI
+from fastapi import FastAPI, BackgroundTasks
 from fastapi.responses import FileResponse
 
 from ..main import generate_from_prompt
@@ -12,3 +13,20 @@ JOBS = {}
 
 os.makedirs("../tesseract/outputs", exist_ok=True)
 
+@app.post("/generate")
+def generate_endpoint(prompt:str, fmt:str = 'ply', background_tasks : BackgroundTasks = None):
+
+    job_id = str(uuid.uuid4())
+    JOBS[job_id] ={"status" : "queued" ,
+                   "file": None}
+    
+    def run_generation():
+        JOBS[job_id]["status"] = "running"
+        file_path = generate_from_prompt(prompt=prompt ,
+                                         formats=fmt)
+        JOBS[job_id]["status"] = "done"
+        JOBS[job_id]["file"] = "saved_files"
+
+    background_tasks.add_task(run_generation)
+
+    return {"job_id" : job_id, "status" : "queued"}
