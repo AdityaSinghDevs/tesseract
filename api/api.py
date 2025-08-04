a2f83e52630334bff8aa83117156c75b3b06ffa7
AdityaSinghDevs <adityasingh.devs@gmail.com>
2025-08-04 23:42:45 +0530

    status check added

--- api/api.py
+++ api/api.py
@@ -1,32 +1,43 @@
-from typing import Union
+from typing import Union, Dict
 import os
 import uuid
 
 from fastapi import FastAPI, BackgroundTasks
 from fastapi.responses import FileResponse
 
-from ..main import generate_from_prompt
+from ..main import generate_from_prompt, BASE_FILE, OUTPUT_DIR
 
 app = FastAPI()
 
-JOBS = {}
+JOBS : Dict[str, Dict] = {} #in memory job store
 
-os.makedirs("../tesseract/outputs", exist_ok=True)
+os.makedirs(OUTPUT_DIR, exist_ok=True) #making surre ouput dir exists
 
 @app.post("/generate")
 def generate_endpoint(prompt:str, fmt:str = 'ply', background_tasks : BackgroundTasks = None):
 
-    job_id = str(uuid.uuid4())
-    JOBS[job_id] ={"status" : "queued" ,
-                   "file": None}
+    job_id = str(uuid.uuid4()) #creatin a random id
+    JOBS[job_id] ={"status" : "queued" , #initializing its  initial values
+                   "files": []}
     
-    def run_generation():
-        JOBS[job_id]["status"] = "running"
-        file_path = generate_from_prompt(prompt=prompt ,
-                                         formats=fmt)
-        JOBS[job_id]["status"] = "done"
-        JOBS[job_id]["file"] = "saved_files"
+    def run_generation(): #fn made to run
+        JOBS[job_id]["status"] = "running" #status updated
+        result = generate_from_prompt(prompt=prompt ,
+                                         formats= fmt,  base_file=BASE_FILE,
+                                         output_dir=OUTPUT_DIR,
+                                         )
+        
+        JOBS[job_id]["status"] = "done" #result info saved
+        JOBS[job_id]["files"] = result["saved_files"]
 
     background_tasks.add_task(run_generation)
 
     return {"job_id" : job_id, "status" : "queued"}
+
+@app.get("/status/{job_id}")
+def check_status(job_id : str):
+    job = JOBS.get(job_id)
+    if not job:
+        return {"error" : "job not found" }
+    return job
+
